94d2f5a977de1cfd2f83108eeaa64a227476aac3
Rayen Gonzalez <rayen.gonzalez@gmail.com>
2017-01-27 11:53:44 +0100

    Update BTree.java
    insert works properly for all cases

--- BTree.java
+++ BTree.java
@@ -45,30 +45,102 @@ public class BTree {
         return false;
     }
 
+    public BTreeNode parent;
     public void insert(int key) {
         if (root == null || root.getKeys() == null) {
             root = new BTreeNode(degree);
             root.addKey(key);
             return;
         } else {
-            if (root.getKeys().size() <= 2 * degree) {
-                int hasBeenAdded = 0;
+            boolean isgtFirstChild = true;
+            if (root.getChildren().size() != 0) {
+                int firstChildLength = root.getChildren().get(0).getKeys().size();
+                for (int i = 0; i < firstChildLength; i++) {
+                    if (key <= root.getChildren().get(0).getKeys().get(i)) {
+                        isgtFirstChild = false;
+                    }
+                }
+            }
+            if (root.getKeys().size() <= 2 * degree && isgtFirstChild == true && root.getChildren().size() != root.getKeys().size() + 1) {
+                boolean hasBeenAdded = false;
                 for (int i = 0; i < root.getKeys().size(); i++) {
                     if (key < root.getKeys().get(i)) {
                         root.getKeys().add(i, key);
-                        hasBeenAdded = 1;
+                        hasBeenAdded = true;
                         break;
                     }
                 }
-                if (hasBeenAdded == 0) {
+                if (hasBeenAdded == false) {
                     root.getKeys().add(key);
                 }
                 if (root.getKeys().size() <= 2 * degree) {
                     return;
                 } else {
+                    int median = (root.getKeys().size()) / 2;
+                    if (parent != null) {
+                        hasBeenAdded = false;
+                        int index = 0;
 
+                        if (parent.getKeys().size() <= 2* degree) {
+                            for (int i = 0; i < parent.getKeys().size(); i++) {
+                                if (root.getKeys().get(median) < parent.getKeys().get(i)) {
+                                    parent.getKeys().add(i, root.getKeys().get(median));
+                                    root.getKeys().remove(median);
+                                    index = i;
+                                    hasBeenAdded = true;
+                                    break;
+                                }
+                            }
+                            if (hasBeenAdded == false) {
+                                root.getKeys().add(key);
+                            }
+                        }
+                        BTreeNode child = root;
+                        this.root = parent;
+
+                        BTreeNode leftLeaf = new BTreeNode(degree);
+                        for (int i = 0; i < median; i++){
+                            leftLeaf.addKey(child.getKeys().get(0));
+                            child.getKeys().remove(0);
+                        }
+                        root.getChildren().add(index, leftLeaf);
+
+                        BTreeNode rightLeaf = new BTreeNode(degree);
+                        int length = child.getKeys().size();
+                        for (int i = 0; i < length; i++){
+                            rightLeaf.addKey(child.getKeys().get(0));
+                            child.getKeys().remove(0);
+                        }
+                        root.getChildren().add(index + 1, rightLeaf);
+
+                        root.getChildren().remove(index + 2);
+                        return;
+
+                    } else {
+
+                        root.addChild(new BTreeNode(degree));
+                        for (int i = 0; i < median; i++) {
+                            root.getChildren().get(0).addKey(root.getKeys().get(0));
+                            root.getKeys().remove(0);
+                        }
+
+                        root.addChild(new BTreeNode(degree));
+                        for (int i = 1; i <= root.getKeys().size(); i++) {
+                            root.getChildren().get(1).addKey(root.getKeys().get(1));
+                            root.getKeys().remove(1);
+                        }
+                    }
                 }
             } else {
+                for (int i = 0; i < root.getKeys().size(); i++) {
+                    if (key < root.getKeys().get(i)) {
+                        BTree child = new BTree(degree);
+                        child.setRoot(root.getChildren().get(i));
+                        child.parent = this.root;
+                        child.insert(key);
+                        return;
+                    }
+                }
 
             }
         }
